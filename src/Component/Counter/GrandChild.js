80360105c392518ed85603960a9fbb78ff27f0cb
Jharna203 <jharnakhatun203@gmail.com>
2022-12-05 10:02:13 +0600

    first commit

---
+++ src/Component/Counter/GrandChild.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GrandChild = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default GrandChild;
---
+++ src/Component/Counter/Parent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Parent = () => {
+    return (
+        <div>
+            <div className="bg-red-600">
+                <h1>0</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Parent;
